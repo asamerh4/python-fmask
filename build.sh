65775b8e3f32ac1dea71b73e8e7cdbad6707236c
Hubert Asamer <hubert.asamer@dlr.de>
2017-02-17 22:28:47 +0000

    updated docs

--- build.sh
+++ build.sh
@@ -9,5 +9,4 @@ NC='\033[0m' # No Color
 #BUILD
 docker build -t asamerh4/python-fmask:fmask0.4-aws-$TAG .
 
-echo -e ${YELLOW}"**build finished -> use: docker run --rm --memory=12g --memory-swap=12g -e inputId=s3://s2-sync/tiles/33/X/WH/2016/9/9/1/ -e outputId=s3://s2-derived/tiles/33/X/WH/2016/9/9/1/ -it asamerh4/python-fmask:fmask0.4-aws-$TAG ./run-fmask.sh" ${NC}
-
+echo -e ${YELLOW}"**build finished -> use: docker run --rm --memory=12g --memory-swap=12g -e inputId=s3://s2-sync/tiles/33/X/WH/2016/9/9/1/ -e outputId=s3://s2-derived/tiles/33/X/WH/2016/9/9/1/ -e AWS_DEFAULT_REGION=eu-central-1 -it asamerh4/python-fmask:fmask0.4-aws-$TAG ./run-fmask.sh" ${NC}
--- docs/sampleRun.sh
+++ docs/sampleRun.sh
@@ -2,8 +2,8 @@ docker run \
 --rm \
 --memory=12g \
 --memory-swap=12g \
+-e AWS_DEFAULT_REGION="eu-central-1"
 -e inputId="s3://s2-sync/tiles/33/X/WH/2016/9/9/1/" \
 -e outputId="s3://s2-derived/tiles/33/X/WH/2016/9/9/1/" \
 -it \
-opus-test08.eoc.dlr.de:5000/s2-fmask:fmask0.4.2-snap4.0-v0.2 \
-./run-fmask.sh
+asamerh4/python-fmask:fmask0.4-aws-3b19a7b ./run-fmask.sh
